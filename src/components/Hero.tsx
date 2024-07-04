cb4b2dc15c03e7ab290f0c488fe455928ad9ad7a
malcolm-decuire <malcolm.decuire@gmail.com>
2024-07-04 19:25:02 -0400

    edited svgs of skills to be smaller

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -58,40 +58,47 @@ export function Hero() {
         </Button>
       </div>
       <div className="mt-36 lg:mt-44">
-        <p className="font-display text-base text-slate-900">
-          Technical Skills
-        </p>
+  <p className="font-display text-base text-slate-900">
+    Technical Skills
+  </p>
+  <ul
+    role="list"
+    className="mt-8 flex items-center justify-center gap-x-8 sm:flex-col sm:gap-x-0 sm:gap-y-10 xl:flex-row xl:gap-x-12 xl:gap-y-0"
+  >
+    {[
+      [
+        { name: 'AWS', logo: aws },
+        { name: 'Excel', logo: excel },
+        { name: 'GCP', logo: gcp },
+      ],
+      [
+        { name: 'OpenAI', logo: openai },
+        { name: 'Python', logo: python },
+        { name: 'SQL', logo: sql },
+      ],
+    ].map((group, groupIndex) => (
+      <li key={groupIndex}>
         <ul
           role="list"
-          className="mt-8 flex items-center justify-center gap-x-8 sm:flex-col sm:gap-x-0 sm:gap-y-10 xl:flex-row xl:gap-x-12 xl:gap-y-0"
+          className="flex flex-col items-center gap-y-8 sm:flex-row sm:gap-x-12 sm:gap-y-0"
         >
-          {[
-            [
-              { name: 'AWS', logo: aws},
-              { name: 'Excel', logo: excel },
-              { name: 'GCP', logo: gcp },
-            ],
-            [
-              { name: 'OpenAI', logo: openai },
-              { name: 'Python', logo: python },
-              { name: 'SQL', logo: sql },
-            ],
-          ].map((group, groupIndex) => (
-            <li key={groupIndex}>
-              <ul
-                role="list"
-                className="flex flex-col items-center gap-y-8 sm:flex-row sm:gap-x-12 sm:gap-y-0"
-              >
-                {group.map((company) => (
-                  <li key={company.name} className="flex">
-                    <Image src={company.logo} alt={company.name} unoptimized />
-                  </li>
-                ))}
-              </ul>
+          {group.map((company) => (
+            <li key={company.name} className="flex">
+              <Image
+                src={company.logo}
+                alt={company.name}
+                unoptimized
+                width={company.logo.width * 0.5} // Scale down width by 20%
+                height={company.logo.height * 0.5} // Scale down height by 20%
+              />
             </li>
           ))}
         </ul>
-      </div>
+      </li>
+    ))}
+  </ul>
+</div>
+
     </Container>
   )
 }
